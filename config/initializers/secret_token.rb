58c7b0a97e63ad966fd6bc28dfc45533b5adbdeb
hyngyn <hyngyn@gmail.com>
2012-09-19 21:08:30 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImageSearch::Application.config.secret_token = '3de120f8947ca62e8027a1c4623b286a1e1f665fdcb2bfe347d5d8e28e0895877fb97fa4a853c112cf4a2d0ad75aadd13c9836a5719d6c5cf85335be49a89664'

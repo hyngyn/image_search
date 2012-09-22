faaa53f2459e340e07c22519f7ccec79b41ac177
hyngyn <hyngyn@gmail.com>
2012-09-22 16:31:27 -0700

    Modal loads, but have to fix issue with images

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,2 +1,11 @@
 module PhotosHelper
+
+  def flickr_url(photo)
+    "http://farm#{photo.farm.to_s}.static.flickr.com/#{photo.server.to_s}/#{photo.id.to_s}_#{photo.secret.to_s}_m.jpg"
+  end
+
+  def modal_url(photo)
+    "http://farm#{photo.farm.to_s}.static.flickr.com/#{photo.server.to_s}/#{photo.id.to_s}_#{photo.secret.to_s}_z.jpg"
+  end
+
 end

a1501575aca891296f7b535879ef443e4258ad33
hyngyn <hyngyn@gmail.com>
2012-09-22 21:16:17 -0700

    Done playing with modals for tonight

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,10 +7,6 @@ class PhotosController < ApplicationController
       @title = params[:search]
       @photos = flickr.photos.search(:tags => params[:search], :has_geo => 1)
     end
- #   @info = []
- #   @photos.each do |photo|
- #     @info << flickr.photos.getInfo(:photo_id => photo.id)
- #   end
 
   end
 

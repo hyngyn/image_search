1426d8a51bd4cc4755ca594b874c386cea7d3937
hyngyn <hyngyn@gmail.com>
2012-09-25 21:41:28 -0700

    Working Google Maps

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -13,17 +13,37 @@
           data: { 'id' : fetch_id, 'secret' : fetch_secret },
           success: function(data){
 
-            console.log(data)
-            console.log(data.title)
- 
-
+               var google_map_url = 'http://maps.googleapis.com/maps/api/staticmap?center=' + data.location.latitude + ',' + data.location.longitude + '&zoom=14&size=400x300&sensor=false';          
+             
             //edit innerHTML of basic_modal
             $('.basic_modal').html(
-              "<div id='modal_image'><%= escape_javascript(image_tag photo[:url]) %></div><div id='photo_info_container'>" + data.title + "</div>"
+
+              "<div id='googlemap_image'>"+
+                "<%# escape_javascript(image_tag google_map_url )%>"+ //THIS LINE IS BROKEN
+              "</div>" +
+
+              "<div id='modal_image'>"+
+                "<%= escape_javascript(image_tag photo[:url]) %>"+
+              "</div>"+
+
+              "<div id='modal_photo_info_container'>"+
+                 "<div class='modal_photo_attr'>"
+                    +data.title+ 
+                 "</div>"+ 
+                 "<div class='modal_photo_attr'>"+
+                    "By: " +data.owner.username+ 
+                 "</div>"+ 
+                 "<div class='modal_photo_attr'>"
+                    +data.location.region._content+ ", " +data.location.country._content+ 
+                 "</div>"+
+              "</div>"
+                 
+
+
              );
 
             //load modal
-	          $('.basic_modal').modal({
+            $('.basic_modal').modal({
               overlayClose:true
             });
 

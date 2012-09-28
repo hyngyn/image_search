a2c27b2b58b0cf983e017f1a79c88c26401ff05e
hyngyn <hyngyn@gmail.com>
2012-09-27 20:02:48 -0700

    Working InfiniteScroll with Modal

--- app/assets/javascripts/photos_masonry.js
+++ app/assets/javascripts/photos_masonry.js
@@ -1,37 +1,48 @@
-      $(function () {
+/* Masonry/InfiniteScroll for Pinterest-like Layout */
 
-        var $container = $('#photos_container');
+$(function () {
 
-        $container.imagesLoaded(function(){
-          $container.masonry({
-           itemSelector : '.each_photo_container',
-          });
+  var $container = $('#photos_container');
+
+  $container.imagesLoaded(function(){
+    $container.masonry({
+     itemSelector : '.each_photo_container',
+    });
+  });
+        
+        
+ 
+  $container.infinitescroll({
+    navSelector  : '#page_navigation',            
+            // selector for the paged navigation (it will be hidden)
+    nextSelector : '#next_button a',    
+            // selector for the NEXT link (to page 2)
+    itemSelector : '.each_photo_container'          
+             // selector for all items you'll retrieve
+
+  },
+  //trigger masonry on infinitescroll callback and add id to div
+    function(newElements){
+      var $newElems = $(newElements).css({ opacity: 0 });
+      $newElems.imagesLoaded(function(){
+        $newElems.attr('id', this.id);
+        $newElems.attr('data-id', this.id);
+        $newElems.attr('data-secret', this.secret);
+        $newElems.attr('data-server', this.server);
+        $newElems.attr('data-farm', this.farm);
+
+        //reload photos_modal.js to add click listener on new elements
+        $.ajax({
+          url: 'assets/photos_modal.js',
+          dataType: "script",
         });
-        
-        
-       /*
-        $container.infinitescroll({
-          navSelector  : 'div.#page_navigation',            
-                  // selector for the paged navigation (it will be hidden)
-          nextSelector : 'div.#page_navigation a',    
-                  // selector for the NEXT link (to page 2)
-          itemSelector : '.each_photo_container'          
-                   // selector for all items you'll retrieve
 
-        },
-        //trigger masonry on infinitescroll callback and add id to div
-          function(newElements){
-            var $newElems = $(newElements).css({ opacity: 0 });
-            $newElems.imagesLoaded(function(){
-              $newElems.animate({ opacity: 1 });
-              $newElems.attr('id', this.id);
-              $container.masonry('appended', $newElems, true);
-
-
-            });
-          }
-
-        );
-       */
+        $newElems.animate({ opacity: 1 });
+        $container.masonry('appended', $newElems, true);
 
       });
+    }
+
+  );
+ 
+});
